3bced7ca7bd6c8d3924b6885f1581e946d4b92f1
xxxgitone <xxxgit@sina.com>
2017-12-15 14:32:30 +0800

    完善事件的发布订阅

--- eventEmitter/eventEmittrt.js
+++ eventEmitter/eventEmittrt.js
@@ -1,6 +1,8 @@
 (function() {
+  var root = (typeof self == 'object' && self.self == self && self) ||
+  (typeof global == 'object' && global.global == global && global) || this || {}
 
-  function isValidListener (listener) {
+  function _isValidListener (listener) {
     if (typeof listener === 'function') {
       return true
     } else if (listener && typeof listener === 'object') {
@@ -10,26 +12,28 @@
     }
   }
 
-  function indexOf (array, item) {
-    if (Array.indexOf) {
-      return Array.indexOf(item)
-    } else {
-      var result = -1
-      for (var i = 0, len = array.length; i < len; i++) {
-        if (array[i] === item) {
-          result = i
-          break
-        }
+  function _indexOf (array, item) {
+    var result = -1
+    item = typeof item === 'object' 
+      ? item.listener 
+      : item
+    
+    for (var i = 0, len = array.length; i < len; i++) {
+      if (array[i].listener === item) {
+        result = i
+        break
       }
-      return result
     }
+
+    return result
   }
 
   function EventEmitter () {
     this._events = {}
   }
 
-  var _proto = EventEmitter.prototype
+
+  var proto = EventEmitter.prototype
 
   /**
    * 
@@ -37,24 +41,24 @@
    * @param {Function} listener 监听器函数
    * @return {Object} 链式
    */
-  _proto.on = function (eventName, listener) {
+  proto.on = function (eventName, listener) {
     if (!eventName || !listener) return
-    
-    if (!isValidListener(listener)) {
+    if (!_isValidListener(listener)) {
       throw new TypeError('listener must be a function')
     }
 
     var events = this._events
-    var listeners = events[eventName] = events[eventName] || []
+    var listeners  = events[eventName] = events[eventName] || []
     var listenerIsWrapped = typeof listener === 'object'
 
     // 避免重复添加
-    if (indexOf(listeners, listener) === -1) {
+    if (_indexOf(listeners, listener) === -1) {
       listeners.push(listenerIsWrapped ? listener : {
         listener: listener,
         once: false
       })
     }
+    
     return this
   }
 
@@ -64,7 +68,7 @@
    * @param {Function} listener 监听器函数
    * @return {Object}
    */
-  _proto.once = function (eventName, listener) {
+  proto.once = function (eventName, listener) {
     return this.on(eventName, {
       listener: listener,
       once: true
@@ -77,19 +81,19 @@
    * @param  {Function} listener 监听器函数
    * @return {Object} 可链式调用
    */
-  _proto.off = function (eventName, listener) {
+  proto.off = function (eventName, listener) {
     var listeners = this._events[eventName]
     if (!listeners) return
 
     var index
-    for (var i = 0, len = listeners.lenght; i < len; i++) {
+    for (var i = 0, len = listeners.length; i < len; i++) {
       if (listeners[i] && listeners[i].listener === listener) {
         index = i
         break
       }
     }
 
-    if (typeof index !== 'undefined') {
+    if (index !== 'undefined') {
       listeners.splice(index, 1, null)
     }
 
@@ -102,11 +106,11 @@
    * @param  {Array} args 传入监听器函数的参数，使用数组形式传入
    * @return {Object} 可链式调用
    */
-  _proto.emit = function (eventName, args) {
+  proto.emit = function (eventName, args) {
     var listeners = this._events[eventName]
     if (!listeners) return
 
-    for (var i = 0, len = listeners.lenght; i < len; i++) {
+    for (var i = 0, len = listeners.length; i < len; i++) {
       var listener = listeners[i]
       if (listener) {
         listener.listener.apply(this, args || [])
@@ -119,5 +123,24 @@
     return this
   }
 
+  /**
+   * 删除某一个类型的所有事件或者所有事件
+   * @param  {String[]} eventName 事件名称
+   */
+  proto.allOff = function (eventName) {
+    if (eventName) {
+      this._events[eventName] = []
+    } else {
+      this._events = {}
+    }
+  }
+
+  if (typeof exports != 'undefined' && !exports.nodeType) {
+    if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+      exports = module.exports = EventEmitter
+    }
+  } else {
+    root.EventEmitter = EventEmitter
+  }
 
 })()
