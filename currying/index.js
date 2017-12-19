bf4168139fe02212d65d1710e8540c00e1deef2d
xxxgitone <xxxgit@sina.com>
2017-12-19 11:14:58 +0800

    柯里化函数

--- currying/index.js
+++ currying/index.js
@@ -12,13 +12,13 @@
 // addCurry(1)(3) // 3
 
 // TODO
-var curry = function (fn) {
-  var ags = [].slice.call(arguments, 1)
-  return function() {
-    var newArgs = args.concat([].slice.call(arguments))
-    return fn.apply(this, newArgs)
-  }
-}
+// var curry = function (fn) {
+//   var ags = [].slice.call(arguments, 1)
+//   return function() {
+//     var newArgs = args.concat([].slice.call(arguments))
+//     return fn.apply(this, newArgs)
+//   }
+// }
 
 // function add(a, b) {
 //   return a + b;
@@ -32,3 +32,35 @@ var curry = function (fn) {
 // //或者
 // var addCurry = curry(add);
 // addCurry(1, 2) // 3
+
+function sub_curry (fn) {
+  var args = [].slice.call(arguments, 1)
+  return function () {
+    return fn.apply(this, args.concat([].slice.call(arguments)))
+  }
+}
+
+// TODO:慢慢消化，有点难理解
+function curry (fn, length) {
+  length = length || fn.length
+
+  var slice = Array.prototype.slice
+
+  return function () {
+    if (arguments.length < length) {
+      var combined = [fn].concat(slice.call(arguments))
+      return curry(sub_curry.apply(this, combined), length - arguments.length)
+    } else {
+      return fn.apply(this, arguments)
+    }
+  }
+}
+
+var fn = curry(function(a, b, c) {
+  return [a, b, c]
+})
+
+console.log(fn("a", "b", "c")) // ["a", "b", "c"]
+console.log(fn("a", "b")("c")) // ["a", "b", "c"]
+console.log(fn("a")("b")("c")) // ["a", "b", "c"]
+console.log(fn("a")("b", "c")) // ["a", "b", "c"]
